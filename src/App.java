ecf68708d7de6eed08ed37624d0bd376fe20d029
Felipe <felipereina1997@hotmail.com>
2025-10-23 19:05:15 -0500

    Actualizacion archivo Other.java

--- src/App.java
+++ src/App.java
@@ -1,3 +1,21 @@
+/*
+ # Inicializas el repositorio local
+git init
+
+# Agregas todos los archivos a seguimiento
+git add .
+
+# Haces el primer commit con un mensaje
+git commit -m "Primer commit"
+
+# Enlazas tu repositorio local con el remoto en GitHub
+git remote add origin https://github.com/TU_USUARIO/NOMBRE_DEL_REPO.git
+
+# Subes el contenido local al remoto (la primera vez)
+git branch -M main
+git push -u origin main
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
--- src/Other.java
+++ src/Other.java
@@ -1,7 +1,29 @@
+/*
+# Verificás en qué rama estás
+git branch
+
+# Crear una nueva rama para tu feature
+git checkout -b feature/nueva-funcionalidad
+
+# Hacés tus cambios en el código...
+
+# Verifico que archivos han sido modificados con mis update
+git status 
+
+# Luego guardás los cambios
+git add .
+git commit -m "Agrega nueva funcionalidad X"
+
+# Subís la rama nueva al remoto
+git push -u origin feature/nueva-funcionalidad
+
+ */
+
 public class Other {
     
     public static void main(String[] args) {
         
         System.out.println("Holita");
+        System.out.println("Esto es nuevo de cerrar y abrir por X tiempo");
     }
 }
